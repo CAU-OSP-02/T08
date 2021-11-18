d4ce0d9a4d00dfe2010b66124bef0eba09188aee
Chansik Kang <chansik4294@cau.ac.kr>
2021-11-18 14:39:12 +0900

    강찬식.txt

--- Gameboard.py
+++ Gameboard.py
@@ -5,6 +5,32 @@ BLACK=(0,0,0)
 pad_width=1024
 pad_height=512
 
+class Ground(pygame.sprite.Sprite):
+       
+       def __init__(self, col, row):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.grid_x = row * 64
+        self.grid_y = col * 64
+        self.fill(BLACK)
+        self.rect = self.image.get_rect()
+
+        self.rect.x = self.grid_x
+        self.rect.y = self.grid_y
+
+class Wall(pygame.sprite.Sprite):
+    
+    def __init__(self, col, row):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.grid_x = row * 64
+        self.grid_y = col * 64
+        self.fill(WHITE)
+        self.rect = self.image.get_rect()
+
+        self.rect.x = self.grid_x
+        self.rect.y = self.grid_y
+        
 def runGame():
     global gamepad, clock
 
